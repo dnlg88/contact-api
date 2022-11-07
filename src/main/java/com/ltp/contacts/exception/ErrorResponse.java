2af06347b550838a40503bc46f092038b0acc720
dnlg88 <92231783+dnlg88@users.noreply.github.com>
2022-11-07 16:39:57 +0100

    Add validations and tests

--- src/main/java/com/ltp/contacts/exception/ErrorResponse.java
+++ src/main/java/com/ltp/contacts/exception/ErrorResponse.java
@@ -1,25 +1,18 @@
 package com.ltp.contacts.exception;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 public class ErrorResponse {
 
-    private String message;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
     private LocalDateTime timestamp;
+    private List<String> message;
 
-    public ErrorResponse(String message) {
+    public ErrorResponse(List<String> message) {
         this.timestamp = LocalDateTime.now();
-        this.message = message;
-    }
-
-    public String getMessage() {
-        return this.message;
-    }
-
-    public void setMessage(String message) {
         this.message = message;
     }
 
@@ -29,6 +22,14 @@ public class ErrorResponse {
 
     public void setTimestamp(LocalDateTime timestamp) {
         this.timestamp = timestamp;
+    }
+
+    public List<String> getMessage() {
+        return this.message;
+    }
+
+    public void setMessage(List<String> message) {
+        this.message = message;
     }
 
 }
--- src/main/java/com/ltp/contacts/pojo/Contact.java
+++ src/main/java/com/ltp/contacts/pojo/Contact.java
@@ -2,11 +2,28 @@ package com.ltp.contacts.pojo;
 
 import java.util.UUID;
 
+import javax.validation.constraints.NotBlank;
+
 public class Contact {
 
     private String id;
+
+    @NotBlank(message = "Name cannot be blank")
     private String name;
+
+    @NotBlank(message = "Phone number cannot be blank")
     private String phoneNumber;
+
+    public Contact(String id, String name, String phoneNumber) {
+        this.id = id;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Contact(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
 
     public Contact() {
         this.id = UUID.randomUUID().toString();
